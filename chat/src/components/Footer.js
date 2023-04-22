dcdb9d8d85b5ea9408170d6c80dc18b6140207ec
leenatantawy <102835003+leenatantawy@users.noreply.github.com>
2023-04-22 13:56:57 -0500

    changes

--- chat/src/components/Footer.js
+++ chat/src/components/Footer.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="pt-5">
-      <img className={"home-image"} src={"Hero-uchicago-campus.jpeg"} />
-      <div className="container text-center">
-        <p>&copy; Chatty 2020.</p>
-      </div>
+    <footer>
+        <div className="container text-center">
+          <p>&copy; Chatty 2020.</p>
+        </div>
     </footer>
   )
 }
--- chat/src/pages/Home.js
+++ chat/src/pages/Home.js
@@ -18,6 +18,7 @@ export default class HomePage extends Component {
             </div>
           </div>
         </section>
+        <Footer></Footer>
       </div>
     )
   }
